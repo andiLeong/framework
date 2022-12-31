457718d8dcc05068a87d0f3da273446e62e119e3
andiLeong <alwayssmile20150724@gmail.com>
2022-12-31 20:16:29 +0800

    bump validation package version

--- core/Facades/Route.php
+++ core/Facades/Route.php
@@ -6,7 +6,7 @@ use Andileong\Framework\Core\Routing\Router;
 
 /**
  * @method static get(string $string, \Closure|array $param)
- * @method static post(string $string, string[] $array)
+ * @method static post(string $string, \Closure|array $array)
  * @method static middleware(string|array $string)
  * @method static put(string $string, string[] $array)
  * @method static delete(string $string, string[] $array)
