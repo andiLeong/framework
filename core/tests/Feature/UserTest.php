e43f28e0524d6af174c4ac5fbbf1088d23118c36
andiLeong <alwayssmile20150724@gmail.com>
2023-02-02 09:32:24 +0800

    fix Usertest create user

--- core/tests/Feature/UserTest.php
+++ core/tests/Feature/UserTest.php
@@ -42,7 +42,7 @@ class UserTest extends ApplicationTestCase
     /** @test */
     public function it_can_create_a_user()
     {
-        $user = User::first();
+        $user = $this->createUser();
         $email = 'uniqueemail@gmail.com';
         $attributes = $this->baseAttribute([
             'email' => $email
